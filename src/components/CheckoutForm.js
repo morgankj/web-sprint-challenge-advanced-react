6f07019b209b59081e260ca478196a2eb961374f
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-03 11:08:44 -0600

    Updated useForm and Checkout form with custom hook

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from '../hooks/useForm';
 
 const initialValue = {
   firstName: "",
@@ -12,21 +13,9 @@ const initialValue = {
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
-const useForm = (initialValue) => {
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = event => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  return [values, handleChanges];
-}
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, handleChanges] = useForm(initialValue);
 
   const handleSubmit = (e) => {
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = event => {
+        setValues({
+        ...values,
+        [event.target.name]: event.target.value
+        })
+    }
+
+    return [values, handleChanges];
+}
+
+export default useForm;
